27a3e28e71ed38dd638be26451f3d10077c68843
nithiyasuresh <nithiyasuresh@gmail.com>
2021-09-10 06:46:53 +1000

    adding choropleth

--- life_exp/static/js/app_new.js
+++ life_exp/static/js/app_new.js
@@ -172,3 +172,32 @@ d3.csv('https://raw.githubusercontent.com/nithiyasuresh/LifeExpectancy_Project/n
     });
 
 });
+
+// 3rd visualisation - world map
+
+d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2010_alcohol_consumption_by_country.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
+
+    var data = [{
+        type: 'choropleth',
+        locationmode: 'country names',
+        locations: unpack(rows, 'location'),
+        z: unpack(rows, 'alcohol'),
+        text: unpack(rows, 'location'),
+        autocolorscale: true
+    }];
+
+    var layout = {
+      title: 'Pure alcohol consumption<br>among adults (age 15+) in 2010',
+      geo: {
+          projection: {
+              type: 'robinson'
+          }
+      }
+    };
+
+    Plotly.newPlot("choropleth", data, layout, {showLink: false});
+
+});
