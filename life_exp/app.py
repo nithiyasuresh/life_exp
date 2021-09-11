1e7473b04304d6bb38a3137bfdf2397363c48338
nithiyasuresh <nithiyasuresh@gmail.com>
2021-09-11 21:23:07 +1000

    trying flask

--- life_exp/app.py
+++ life_exp/app.py
@@ -1,5 +1,7 @@
 # import necessary libraries
 import os
+import sqlite3
+from typing import Counter
 from flask import (
     Flask,
     render_template,
@@ -24,56 +26,81 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-from .models import Pet
+from .models import Life
 
+#define database GET Requests
+def get_db():
+    conn = sqlite3.connect(Life)
+    return conn
+
+# def get_by_country(country):
+#     db = get_db()
+#     cursor = db.cursor()
+#     statement = "SELECT * FROM country_data WHERE Country LIKE ?"
+#     cursor.execute(statement, [country])
+#     return cursor.fetchall()
+
+
+def get_all():
+    db = get_db()
+    cursor = db.cursor()
+    query = "SELECT * FROM LIFE"
+    cursor.execute(query)
+    return cursor.fetchall()
 
 # create route that renders index.html template
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# define App routes to call GET Methods
+@app.route('/countries', methods=["GET"])
+def get_all_countries():
+    countries = get_all()
+    return jsonify(countries)
+
+
 
 # Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
+# @app.route("/send", methods=["GET"])
+# def send():
+#     if request.method == "GET":
+#         name = request.form["petName"]
+#         lat = request.form["petLat"]
+#         lon = request.form["petLon"]
 
-        pet = Pet(name=name, lat=lat, lon=lon)
-        db.session.add(pet)
-        db.session.commit()
-        return redirect("/", code=302)
+#         life = Life(Country, lat=lat, lon=lon)
+#         db.session.add(life)
+#         db.session.commit()
+#         return redirect("/", code=302)
 
-    return render_template("form.html")
+#     return render_template("form.html")
 
+# @app.route("/api/pals")
+# def pals():
+#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
 
-@app.route("/api/pals")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+#     hover_text = [result[0] for result in results]
+#     lat = [result[1] for result in results]
+#     lon = [result[2] for result in results]
 
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
+#     pet_data = [{
+#         "type": "scattergeo",
+#         "locationmode": "USA-states",
+#         "lat": lat,
+#         "lon": lon,
+#         "text": hover_text,
+#         "hoverinfo": "text",
+#         "marker": {
+#             "size": 15,
+#             "line": {
+#                 "color": "rgb(8,8,8)",
+#                 "width": 1
+#             },
+#         }
+#     }]
 
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 15,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
-
-    return jsonify(pet_data)
+    # return jsonify(pet_data)
 
 
 if __name__ == "__main__":
--- life_exp/models.py
+++ life_exp/models.py
@@ -1,13 +1,34 @@
 from .app import db
 
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
+class Life(db.Model):
+    __tablename__ = 'Life'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    lat = db.Column(db.Float)
-    lon = db.Column(db.Float)
+    Country = db.Column(db.Float)
+    Year = db.Column(db.Integer)
+    Status = db.Column(db.Float)
+    Life_Expectancy = db.Column(db.Float)
+    Adult_Mortality = db.Column(db.Float)
+    Infant_Deaths = db.Column(db.Float)
+    Alcohol = db.Column(db.Float)
+    Percentage_Expenditure = db.Column(db.Float)
+    Hepatitis_B = db.Column(db.Float)
+    Measles = db.Column(db.Float)
+    BMI = db.Column(db.Float)
+    underfive_deaths = db.Column(db.Float)
+    Polio = db.Column(db.Float)
+    Total_expenditure = db.Column(db.Float)
+    Diphtheria = db.Column(db.Float)
+    Hiv_AIDS = db.Column(db.Float)
+    GDP = db.Column(db.Float)
+    Population = db.Column(db.Float)
+    thinness_1To19years = db.Column(db.Float)
+    thinness_5To9years = db.Column(db.Float)
+    Income_composition_of_resources = db.Column(db.Float)
+    Schooling = db.Column(db.Float)
+    region = db.Column(db.Float)
+    sub_region = db.Column(db.Float)
 
     def __repr__(self):
-        return '<Pet %r>' % (self.name)
+        return '<Life %r>' % (self.name)
